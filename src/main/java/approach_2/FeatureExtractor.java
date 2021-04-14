3f17b37c2376a395897e346bc0c7abd2accfba47
sujanshrestha <shrestha94.sujan@gmail.com>
2021-04-14 12:20:02 -0500

    Splitted FeatureExtractor into two different approaches.

--- src/main/java/approach_2/FeatureExtractor.java
+++ src/main/java/approach_2/FeatureExtractor.java
@@ -1,3 +1,5 @@
+package approach_2;
+
 import org.apache.commons.io.FileUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -13,12 +15,17 @@ import java.util.stream.Collectors;
 
 public class FeatureExtractor {
     private final String FIELD_VALUE = "assignees";
+    private final String ASSOCIATION_MAPPING = "AssociationMappings";
     private final String[] EXTENSION = {"xml"};
-    private List<OMFeature> omFeatures = new ArrayList<>();
+    private List<ObjectModel> objectModels = new ArrayList<>();
+    private List<Association> associations = new ArrayList<>();
     private List<Feature> allOMFeatures = new ArrayList<>();
     private List<Measurement> measurements = new ArrayList<>();
     /* eg: key = customerOrderOM_Sol_2, value = { key = Customer, value = UnionSubClass } */
     private Map<String, Map<String, String>> solToObjectMapStrategy = new HashMap<>();
+
+    /* eg: key = customerOrderOM_Sol_2, value = { key = CustomerOrderAssociation, value = OwnAssociationTable } */
+    private Map<String, Map<String, String>> solToAssociationMapStrategy = new HashMap<>();
     private final String EQUALS = "=";
     private final String SPACE = " ";
     private static final String SLASH = "/";
@@ -34,10 +41,10 @@ public class FeatureExtractor {
         String solutionTxtPath;
         FeatureExtractor fe = new FeatureExtractor();
         ParetoOptimalSolutions ps = new ParetoOptimalSolutions();
-        if (args.length > 2)
-            alloyFileDirectory = args[1];   //"src/main/java/models/customerOrderObjectModel.als"
-            solutionDirectory = args[2];    //"src/main/java/solutions/customer_order/ImplSolution";
-            solutionTxtPath = args[3];      //"src/main/java/solutions/customer_order/customerOrderObjectModel.txt";
+//        if (args.length > 2)
+            alloyFileDirectory = "src/main/java/models/decider.als";
+            solutionDirectory = "src/main/java/solutions/decider/ImplSolution";
+            solutionTxtPath = "src/main/java/solutions/decider/decider.txt";
             String[] tmp = alloyFileDirectory.split(SLASH);
             fe.alsFileName = tmp[tmp.length - 1].split(DOT)[0].concat(CSV);
 
@@ -70,18 +77,18 @@ public class FeatureExtractor {
                     String[] tokens = line.split(SPACE);
                     String object = tokens[2];
 
-                    OMFeature omFeature;
-                    int omFeatureIndex = omFeatures.stream().map(OMFeature::getObject).collect(Collectors.toList()).indexOf(object);
-                    /* If object not in the list of OMFeature objects */
+                    ObjectModel objectModel;
+                    int omFeatureIndex = objectModels.stream().map(ObjectModel::getObject).collect(Collectors.toList()).indexOf(object);
+                    /* If object not in the list of approach_2.OMFeature objects */
                     if (omFeatureIndex == -1) {
-                        omFeature = new OMFeature(object, CLASS);
+                        objectModel = new ObjectModel(object);
                     }
-                    else omFeature = omFeatures.get(omFeatureIndex);
+                    else objectModel = objectModels.get(omFeatureIndex);
 
                     int attributes = 0;
                     boolean hasID = false;
-                    List<OMFeature> parentOMs = new ArrayList<>();
-                    int children = 0;
+                    ObjectModel parentOM = null;
+                    int parents = 0;
 
                     while (!(line = br.readLine()).contains("}")) {
                         if (line.contains("attrSet")) {
@@ -94,31 +101,25 @@ public class FeatureExtractor {
                         }
                         else if (line.contains("parent") && line.contains("in")) {
                             String parent = line.split(SPACE)[2].trim();
-                            OMFeature parentOM = omFeatures.stream().filter(feature -> feature.getObject().equalsIgnoreCase(parent)).findFirst().orElse(new OMFeature(parent, CLASS));
-                            parentOM.setNumChildren(parentOM.getNumChildren() + 1);
-                            parentOMs.add(parentOM);
-
-                            /* Update the children for all parents */
-                            getParents(parentOM, parentOMs);
-
+                            parentOM = objectModels.stream().filter(feature -> feature.getObject().equalsIgnoreCase(parent)).findFirst().orElse(new ObjectModel(parent));
+                            parentOM.setChildren(parentOM.getChildren() + 1);
+                            parents = 1;
+                            parents = getParents(parentOM, parents);
                         }
                     }
 
-                    omFeature.setParent(parentOMs);
-                    omFeature.setNumChildren(children);
-                    omFeature.setAttributes(attributes);
-                    omFeature.setHasID(hasID);
+                    objectModel.setParent(parentOM);
+                    objectModel.setParents(parents);
+                    objectModel.setAttributes(attributes);
+                    objectModel.setHasID(hasID);
 
-                    /* Only add OMFeature object if not already exists */
-                    if (omFeatureIndex == -1) omFeatures.add(omFeature);
+                    /* Only add ObjectModel if not already exists */
+                    if (omFeatureIndex == -1) objectModels.add(objectModel);
                 }
                 else if (line.contains("sig") && line.contains("extends") && line.contains("Association")) {
 
                     String[] tokens = line.split(SPACE);
                     String object = tokens[2];
-
-                    OMFeature omFeature = new OMFeature(object, ASSOCIATION);
-                    omFeatures.add(omFeature);
 
                     String src = "", dst = "", srcMul ="", dstMul = "";
                     while (!(line = br.readLine()).contains("}")) {
@@ -134,44 +135,42 @@ public class FeatureExtractor {
                         else if (line.contains("dst_multiplicity") && line.contains("=")) {
                             dstMul = line.split(EQUALS)[1].trim();
                         }
-
                     }
-                    /* Find the OMFeature for given source */
+
+                    associations.add(new Association(object, src, dst, srcMul, dstMul));
+
+                    /* Find the approach_2.OMFeature for given source */
                     String finalSrc = src;
-                    OMFeature srcOM = omFeatures.stream().filter(feature -> feature.getObject().equalsIgnoreCase(finalSrc)).findFirst().orElse(null);
+                    ObjectModel srcOM = objectModels.stream().filter(feature -> feature.getObject().equalsIgnoreCase(finalSrc)).findFirst().orElse(null);
                     /* If object not found */
                     if (srcOM == null) {
-                        srcOM = new OMFeature(src, CLASS);
-                        omFeatures.add(srcOM);
+                        srcOM = new ObjectModel(src);
+                        objectModels.add(srcOM);
                     }
 
-                    if (srcMul.equalsIgnoreCase("one"))
-                        srcOM.setToOneRelation(srcOM.getToOneRelation() + 1);
-                    else if (srcMul.equalsIgnoreCase("many"))
-                        srcOM.setToManyRelation(srcOM.getToManyRelation() + 1);
+                    if (srcMul.equalsIgnoreCase("one") || srcMul.equalsIgnoreCase("many"))
+                        srcOM.setSrcMultiplicity(true);
 
-                    /* Find the OMFeature for given destination */
+                    /* Find the approach_2.OMFeature for given destination */
                     String finalDst = dst;
-                    OMFeature dstOM = omFeatures.stream().filter(feature -> feature.getObject().equalsIgnoreCase(finalDst)).findFirst().orElse(null);
+                    ObjectModel dstOM = objectModels.stream().filter(feature -> feature.getObject().equalsIgnoreCase(finalDst)).findFirst().orElse(null);
                     /* If object not found */
                     if (dstOM == null) {
-                        dstOM = new OMFeature(dst, CLASS);
-                        omFeatures.add(dstOM);
+                        dstOM = new ObjectModel(dst);
+                        objectModels.add(dstOM);
                     }
 
-                    if (dstMul.equalsIgnoreCase("one"))
-                        dstOM.setToOneRelation(dstOM.getToOneRelation() + 1);
-                    else if (dstMul.equalsIgnoreCase("many"))
-                        dstOM.setToManyRelation(dstOM.getToManyRelation() + 1);
+                    if (dstMul.equalsIgnoreCase("one") || dstMul.equalsIgnoreCase("many"))
+                        dstOM.setDstMultiplicity(true);
                 }
             }
         }
 
-//        omFeatures.forEach(System.out :: println);
+        objectModels.forEach(System.out :: println);
     }
 
     /**
-     * Method to parse the solution xml files and extract information into Feature objects.
+     * Method to parse the solution xml files and extract information into approach_2.Feature objects.
      * @param directory
      * @throws ParserConfigurationException
      * @throws IOException
@@ -182,7 +181,7 @@ public class FeatureExtractor {
         List<File> files = (List<File>) FileUtils.listFiles(filePath, EXTENSION, false);
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
-        /* For each file extract mapping strategy and create Feature objects */
+        /* For each file extract mapping strategy and create approach_2.Feature objects */
         for (File file : files) {
             Document document = db.parse(new FileInputStream(file.getCanonicalPath()));
             document.getDocumentElement().normalize();
@@ -192,6 +191,7 @@ public class FeatureExtractor {
             /* Initialize objectToMapStrategy to new hashmap */
             /* eg: key = Customer, value = UnionSubClass */
             Map<String, String> objectToMapStrategy = new HashMap<>();
+            Map<String, String> associationToMapStrategy = new HashMap<>();
             for (int i = 0; i < fields.getLength(); i++) {
                 Element field = (Element) fields.item(i);
                 if (field.getAttribute("label").trim().equalsIgnoreCase(FIELD_VALUE)) {
@@ -200,17 +200,22 @@ public class FeatureExtractor {
                         Element tuple = (Element) tuples.item(j);
                         NodeList atoms = tuple.getElementsByTagName("atom");
                         Element mappingStrategyElement = (Element) atoms.item(0);
-                        String mappingStrategy = mappingStrategyElement.getAttribute("label").split("/")[1];
+                        String[] mappingStrategy = mappingStrategyElement.getAttribute("label").split("/");
                         Element objectElement = (Element) atoms.item(1);
                         String object = objectElement.getAttribute("label").split("/")[1];
-                        objectToMapStrategy.put(object, mappingStrategy);
+                        if (!mappingStrategy[0].contains(ASSOCIATION_MAPPING)) {
+                            objectToMapStrategy.put(object, mappingStrategy[1]);
+                        } else {
+                            associationToMapStrategy.put(object, mappingStrategy[1]);
+                        }
                     }
                     solToObjectMapStrategy.put(fileName, objectToMapStrategy);
+                    solToAssociationMapStrategy.put(fileName, associationToMapStrategy);
                 }
             }
         }
         // Test
-        /*for (Map.Entry<String, Map<String, String>> entry : solsToObjectMapStrategy.entrySet()) {
+       /* for (Map.Entry<String, Map<String, String>> entry : solToAssociationMapStrategy.entrySet()) {
             System.out.println(entry.getKey());
             for (Map.Entry<String, String> objMapStrat : entry.getValue().entrySet()) {
                 System.out.println(objMapStrat.getKey());
@@ -221,42 +226,74 @@ public class FeatureExtractor {
     }
 
     /**
-     * Recursive method to add all parents for a given Object model
-     * @param parentOM
-     * @param parentOMs
+     * Recursive method to update the number of children for all parents for a given Object model
      */
-    public void getParents(OMFeature parentOM, List<OMFeature> parentOMs) {
-        List<OMFeature> parents = parentOM.getParent();
-        if (parents.size() == 0)
-                return;
-        parentOMs.addAll(parents);
+    public int getParents(ObjectModel om, int parents) {
+        ObjectModel parent = om.getParent();
+        /* Base rule */
+        if (parent == null)
+            return parents;
+        parents++;
         /* Update number of children for each parent */
-        parents.forEach(parent -> parent.setNumChildren(parent.getNumChildren() + 1));
+        parent.setChildren(parent.getChildren() + 1);
         /* Recursive call to getParents */
-        parents.forEach(parent -> getParents(parent, parentOMs));
+        return getParents(parent, parents);
     }
 
     /**
-     * Load Features with attributes such as mappingStrategy and paretoFrontier.
+     * Load Features with attributes such as mappingStrategy, associationStrategies, and paretoFrontier.
      */
     public void loadFeatures() throws IOException {
+        /* For each solution get all the object mappings and association mappings */
         for (Measurement measurement : measurements) {
             String solution = measurement.getSolutionName();
             boolean paretoFrontier = measurement.isParetoFrontier();
+            Set<Feature> solToOMFeatures = new HashSet<>();
             Map<String, String> objToMapStrategy = solToObjectMapStrategy.get(solution);
+            Map<String, String> ascToMapStrategy = solToAssociationMapStrategy.get(solution);
+
+            /* Iterating over Object to Mapping Strategy */
             for (Map.Entry<String, String> entry: objToMapStrategy.entrySet()) {
                 String object = entry.getKey();
                 String mapStrategy = entry.getValue();
-                OMFeature omFeature = omFeatures.stream().filter(feature -> feature.getObject().equalsIgnoreCase(object)).findFirst().orElse(null);
-                if (omFeature != null) {
-                    allOMFeatures.add( new Feature(omFeature, mapStrategy, paretoFrontier));
+                ObjectModel objectModel = objectModels.stream().filter(feature -> feature.getObject().equalsIgnoreCase(object)).findFirst().orElse(null);
+                if (objectModel != null) {
+                    if (objectModel.getParent() != null) {
+                        String parentName = objectModel.getParent().getObject();
+                        for (Map.Entry<String, String> entry1 : objToMapStrategy.entrySet()) {
+                            if (entry1.getKey().equalsIgnoreCase(parentName)) {
+                                solToOMFeatures.add(new Feature(objectModel, mapStrategy, entry1.getValue(), new ArrayList<>(), paretoFrontier));
+                            }
+                        }
+                    } else {
+                        solToOMFeatures.add(new Feature(objectModel, mapStrategy, "none", new ArrayList<>(), paretoFrontier));
+                    }
                 }
+            }
 
+            /* Iterating over Association to Sapping Strategy */
+            for (Map.Entry<String, String> entry : ascToMapStrategy.entrySet()) {
+                String association = entry.getKey();
+                String ascMapping = entry.getValue();
+                Association ascObject =  associations.stream().filter(asc -> asc.getAssociation().equalsIgnoreCase(association)).findFirst().orElse(null);
+                /* Main change goes in here */
+                if (ascObject != null &&
+                        object.equalsIgnoreCase(ascObject.getSrc()) || object.equalsIgnoreCase(ascObject.getDst())) {
+                    allOMFeatures.add( new Feature(objectModel, mapStrategy, ascMapping, paretoFrontier));
+                    hasAssociation = true;
+                }
+            }
+                    boolean hasAssociation = false;
+
+                    if (!hasAssociation)
+                        allOMFeatures.add(new Feature(objectModel, mapStrategy, "none", paretoFrontier));
+                }
             }
         }
 
-        allOMFeatures.forEach(System.out :: println);
+//        allOMFeatures.forEach(System.out :: println);
 
         FileUtil.generateCSVFileWithAllFeatures(allOMFeatures, alsFileName);
+        FileUtil.combine_csv_file();
     }
 }
