b4f04566dca59ec22df5dd7463e99abbd12b9220
sujanshrestha <shrestha94.sujan@gmail.com>
2021-04-14 12:20:21 -0500

    Splitted FileUtils into two different approaches.

--- src/main/java/approach_1/FileUtil.java
+++ src/main/java/approach_1/FileUtil.java
@@ -1,10 +1,17 @@
+package approach_1;
+
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
 import java.util.List;
 
 public class FileUtil {
 
-    private static final String FEATURE_CSV_FILES = "src/main/java/feature_csv_files";
+    private static final String FEATURE_CSV_FILES = "src/main/java/approach_1.feature_csv_files";
+    private static final String COMBINED_FEATURE_CSV_FILES = "src/main/java/approach_1.feature_csv_files/union";
+    private static final String OUTPUT_CSV = "features";
     private static final String DELIMITER = ",";
+    private static final String[] EXTENSION = {"csv"};
 
     public static void generateCSVFileWithAllFeatures(List<Feature> features, String outputFile) throws IOException {
 
@@ -32,6 +39,32 @@ public class FileUtil {
                         paretoFrontier, mapStrategy);
 
                 bw.write(line + "\n");
+            }
+        }
+
+    }
+
+    public static void combine_csv_file() throws IOException {
+        File filePath = new File(COMBINED_FEATURE_CSV_FILES);
+        if (!filePath.exists()) {
+            filePath.mkdir();
+        }
+
+        List<File> files = (List<File>) FileUtils.listFiles(new File(FEATURE_CSV_FILES), EXTENSION, false);
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(COMBINED_FEATURE_CSV_FILES.concat("/")
+                .concat(OUTPUT_CSV)
+                .concat(".")
+                .concat(EXTENSION[0])))) {
+            int fileNum = 0;
+            for (File file: files) {
+                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                    if (fileNum != 0) br.readLine();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        bw.write(line+ "\n");
+                    }
+                }
+                fileNum++;
             }
         }
 
